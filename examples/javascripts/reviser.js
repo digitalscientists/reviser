8109c2e2774d58f5f582025f1fe27b8c456eb7be
Christopher Burnett <signalstatic@gmail.com>
2008-11-21 23:46:28 -0500

    comments, witty when possible

--- examples/javascripts/reviser.js
+++ examples/javascripts/reviser.js
@@ -1,14 +1,18 @@
 var DS = {
 	Reviser:function(el) {
-		// initialization
-		this.editorElement = $(el);
-		this.contentBackup = this.editorElement.html();
-		this.menu = new DS.Menu(this);
+		/* initialization */
+		// the elem we are editing
+		this.editorElement = $(el); 
+		// save the content for revert
+		this.contentBackup = this.editorElement.html(); 
+		// create a new Menu and pass it an editor instance
+		this.menu = new DS.Menu(this); 
 		/*-------- Editor Core ---------------*/
 		this.appendMenuToElement = function() {
 			var coords = this.editorElement.offset();
 			this.menu.attr("id",this.editorElement[0].id + '_reviser');
 			$('body').append(this.menu);
+			// postioning the menu
 			this.menu.css({
 				'position':'absolute',
 				'display':'none',
@@ -18,11 +22,13 @@ var DS = {
 			//this.menu.slideToggle('medium');
 			this.menu.show();
 		};
+		// get rid of the click and start editing
 		this.setElementToEditable = function(){
-			this.editorElement.unbind('click');
+			this.editorElement.unbind('click'); 
 			this.editorElement.attr('contenteditable',true);
 			return true;
 		};
+		// kill the menu and re-bind
 		this.setElementToNonEditable = function(){
 			var menu = $('#'+this.editorElement[0].id + '_reviser');
 			/*menu.slideToggle('medium',function(){
@@ -36,12 +42,16 @@ var DS = {
 			return true;
 		};
 		/*------------------------------------*/
+		
+		// starting up
 		this.appendMenuToElement();
 		this.setElementToEditable();
 	},
 	Menu:function(editor){
-		var editor = editor;
+		// Loading the Commands
 		$.extend(this,DS.Commands);
+		
+		// This should be passed in through a cfg at soem point
 		var menu = $('<div class="reviser_menu">\
 			<a href="#" class="reviser_btn" id="boldSelection" alt="Text Bold">bold</a>\
 			<a href="#" class="reviser_btn" id="italicSelection" alt="Text Italic">italic</a>\
@@ -55,25 +65,34 @@ var DS = {
 			<a href="#" class="reviser_btn" id="save" alt="Save">save</a>\
 			<a href="#" class="reviser_btn" id="revert" alt="Save">cancel</a>\
 		</div>');
+		// Bind that trick
 		this.bindMenu = function(){
+			// Assign scope to Menu
 			var scope = this;
 			$('.reviser_btn',menu).each(function(){
 				$(this).click(function(){
+					// Methods bound to dom elems but scoped
+					// to Menu 
 					scope[this.id].call(scope);
 				});
 			});
 		};
+		// Just a hook for now. Callback should come from cfg.
 		this.save = function(){
 			editor.setElementToNonEditable();
 			console.log(editor.editorElement.html());
 		};
+		// Don't like it, revert it.
 		this.revert = function(){
 			editor.setElementToNonEditable();
 			editor.editorElement.html(editor.contentBackup);
 		};
+		// bind that trick, get 'em
 		this.bindMenu();
 		return menu;
 	},
+	// Base commands for an editable area.
+	// "Borrowed" from wysihat.
 	Commands:{
 		createLink:function() {
 			url = this.needInput("What url? (Use http://)");
